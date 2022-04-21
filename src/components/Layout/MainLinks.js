2ce47bdc4c5eea2d91a408a821c7b55fac3f9657
João de Melo <joaomigueld3@gmail.com>
2022-04-20 21:02:40 -0300

    router and components config and styling

---
+++ src/components/Layout/MainLinks.js
@@ -0,0 +1,69 @@
+import React from "react";
+import {
+  GitPullRequest,
+  AlertCircle,
+  Messages,
+ 
+} from "tabler-icons-react";
+import { ThemeIcon, UnstyledButton, Group, Text } from "@mantine/core";
+import { useNavigate } from "react-router-dom";
+
+function MainLink({ icon, color, label, path }) {
+  const navigate = useNavigate();
+
+  return (
+    <UnstyledButton
+      onClick={() => navigate(path)}
+      sx={(theme) => ({
+        display: "block",
+        width: "100%",
+        padding: theme.spacing.xs,
+        borderRadius: theme.radius.sm,
+        color:
+          theme.colorScheme === "dark" ? theme.colors.dark[0] : theme.black,
+
+        "&:hover": {
+          backgroundColor:
+            theme.colorScheme === "dark"
+              ? theme.colors.dark[6]
+              : theme.colors.gray[0],
+        },
+      })}
+    >
+      <Group>
+        <ThemeIcon color={color} variant="light">
+          {icon}
+        </ThemeIcon>
+
+        <Text size="sm">{label}</Text>
+      </Group>
+    </UnstyledButton>
+  );
+}
+
+const routes = [
+  {
+    icon: <GitPullRequest size={16} />,
+    color: "blue",
+    label: "Home",
+    path: "/",
+  },
+  {
+    icon: <AlertCircle size={16} />,
+    color: "teal",
+    label: "Appointment",
+    path: "/appointment",
+  },
+  
+  
+];
+
+export default function MainLinks() {
+  return (
+    <div>
+      {routes.map((route) => (
+        <MainLink key={route.label} {...route} />
+      ))}
+    </div>
+  );
+}
